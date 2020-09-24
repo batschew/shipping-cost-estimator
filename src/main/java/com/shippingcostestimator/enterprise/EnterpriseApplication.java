3552df8c522c5b3cfb44944e7b878cf85dc8a8c3
Evan Batsch <batschew@mail.uc.edu>
2020-09-24 16:52:45 -0400

    Added initial project files

---
+++ src/main/java/com/shippingcostestimator/enterprise/EnterpriseApplication.java
@@ -0,0 +1,15 @@
+package com.shippingcostestimator.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Starts up Spring Boot instance
+
+@SpringBootApplication
+public class EnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/shippingcostestimator/enterprise/PackageEstimatorController.java
@@ -0,0 +1,16 @@
+package com.shippingcostestimator.enterprise;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // Decides what renders when a user hits a URL or endpoint
+public class PackageEstimatorController {
+    /**
+     * Handle the root ("/") endpoint and return a start page.
+     * @return
+     */
+    @RequestMapping("/")
+    public String index() {
+        return "start";
+    }
+}
