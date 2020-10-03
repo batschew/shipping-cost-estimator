4f1af9b98c483b29a1f7ea99efc9f7e7129ec828
willi6m7 <willi6m7@mail.uc.edu>
2020-10-03 13:38:01 -0400

    M1 DTO update
    Added DAO, DTO, and service packages. Also added the Shipment DTO, which is the same thing as PackageDetails on the UML diagram.

---
+++ src/main/java/com/shippingcostestimator/enterprise/dto/Shipment.java
@@ -0,0 +1,19 @@
+package com.shippingcostestimator.enterprise.dto;
+
+import lombok.Data;
+
+/*
+* DTO responsible for the initial package traits - uses Lombok to handle getters and setters.
+*
+* packageId is going to be the ID for that specific package,
+* carrierId is to be the API ID for specific carriers,
+* predefinedPackId is going to be the predefined ID for a package from a specific carrier.*
+* predefinedPackId may need to be moved to another DTO, however.
+ */
+public @Data
+class Shipment {
+    int packageId;
+    int carrierId;
+    int predefinedPackId;
+    String packageName;
+}
