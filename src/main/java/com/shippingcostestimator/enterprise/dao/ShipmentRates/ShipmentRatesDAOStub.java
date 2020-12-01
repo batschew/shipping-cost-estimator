a7699685d8b8644f19524c8046f6f9ebb0aa2c09
willi6m7 <willi6m7@mail.uc.edu>
2020-12-01 16:38:37 -0500

    Add Stub for DAO
    Adds a stub for the ShipmentRatesDAO Interface.

---
+++ src/main/java/com/shippingcostestimator/enterprise/dao/ShipmentRates/ShipmentRatesDAOStub.java
@@ -0,0 +1,55 @@
+package com.shippingcostestimator.enterprise.dao.ShipmentRates;
+
+import com.shippingcostestimator.enterprise.dto.ShipmentRate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ShipmentRatesDAOStub implements IShipmentRatesDAO{
+
+    Map<Integer, ShipmentRate> allShipmentRates = new HashMap<>();
+
+    /***
+     * Saves a ShipmentRate estimate to a hashmap.
+     * @param shipmentRate
+     * @return Saved shipmentRate estimate.
+     */
+    @Override
+    public ShipmentRate saveRate(ShipmentRate shipmentRate) {
+        int shipmentId = shipmentRate.getId();
+        allShipmentRates.put(shipmentId, shipmentRate);
+        return shipmentRate;
+    }
+
+    /***
+     * Returns a ShipmentRate object with a specified integer ID.
+     * @param id
+     * @return  a specific shipment based on the id.
+     */
+    @Override
+    public ShipmentRate findRateById(int id) {
+        return allShipmentRates.get(id);
+    }
+
+    /***
+     * Returns a list of all ShipmentRate objects that are currently in use by the program.
+     * @return a list of all shipmentRates.
+     */
+    @Override
+    public List<ShipmentRate> findAllRates() {
+        List<ShipmentRate> fetchShipmentRates = new ArrayList(allShipmentRates.values());
+        return fetchShipmentRates;
+    }
+
+    /*
+     * Deletes a ShipmentRate object.
+     */
+    @Override
+    public void delete(int id) {
+        allShipmentRates.remove(id);
+    }
+}
