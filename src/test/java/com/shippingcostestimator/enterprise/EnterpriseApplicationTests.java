1f6d7556e955b776af9d89c827e0eed9372099d4
willi6m7 <willi6m7@mail.uc.edu>
2020-12-01 19:41:20 -0500

    Update Unit Tests
    Updated the unit tests to reflect the backend.

--- src/test/java/com/shippingcostestimator/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/shippingcostestimator/enterprise/EnterpriseApplicationTests.java
@@ -3,14 +3,10 @@ package com.shippingcostestimator.enterprise;
 import com.easypost.EasyPost;
 import com.easypost.exception.EasyPostException;
 import com.easypost.model.Rate;
-import com.easypost.model.Shipment;
-import com.shippingcostestimator.enterprise.dao.IShipmentMapDAO;
-import com.shippingcostestimator.enterprise.dto.FromAddress;
-import com.shippingcostestimator.enterprise.dto.PackageInfo;
-import com.shippingcostestimator.enterprise.dto.ShipmentMap;
-import com.shippingcostestimator.enterprise.dto.ToAddress;
-import com.shippingcostestimator.enterprise.service.IShipmentMapService;
-import com.shippingcostestimator.enterprise.service.ShipmentMapService;
+import com.shippingcostestimator.enterprise.dao.ShipmentRates.IShipmentRatesDAO;
+import com.shippingcostestimator.enterprise.dto.*;
+import com.shippingcostestimator.enterprise.service.ShipmentRates.IShipmentRatesService;
+import com.shippingcostestimator.enterprise.service.ShipmentRates.ShipmentRatesService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,11 +22,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class EnterpriseApplicationTests {
 
-    private IShipmentMapService shipmentMapService;
-    private ShipmentMap shipmentMap = new ShipmentMap();
+    private IShipmentRatesService shipmentRateService;
+    private ShipmentRate shipmentRate = new ShipmentRate();
 
     @MockBean
-    private IShipmentMapDAO shipmentMapDAO;
+    private IShipmentRatesDAO shipmentRateDAO;
 
     @Test
     void contextLoads() {
@@ -39,42 +35,37 @@ class EnterpriseApplicationTests {
     //Checks if fetching a shipment functions correctly.
     @Test
     void fetchShipmentById_returnsShipmentWithId5() throws Exception{
-        givenShipmentMapDataAvailable();
-        whenShipmentWithId5Exists();
-        whenShipmentDataWithId5();
-        thenReturnShipmentWithId5();
+        givenShipmentRateDataAvailable();
+        whenShipmentRateWithId5Exists();
+        whenShipmentRateDataWithId5();
+        thenReturnShipmentRateWithId5();
     }
 
-    private void givenShipmentMapDataAvailable(){
-        Mockito.when(shipmentMapDAO.saveEstimate(shipmentMap)).thenReturn(shipmentMap);
-        shipmentMapService = new ShipmentMapService(shipmentMapDAO);
+    private void givenShipmentRateDataAvailable(){
+        Mockito.when(shipmentRateDAO.saveRate(shipmentRate)).thenReturn(shipmentRate);
+        shipmentRateService = new ShipmentRatesService(shipmentRateDAO);
     }
 
-    private void whenShipmentWithId5Exists(){
+    private void whenShipmentRateWithId5Exists(){
+        EasyPost.apiKey = "EZTK773d7864beb64671aadc6a9d64777a6boAq0h9WzLFA70as0wPnkrQ";
+        ShipmentMap shipmentMap = new ShipmentMap();
         FromAddress fromAddress = new FromAddress();
         ToAddress toAddress = new ToAddress();
         PackageInfo parcel = new PackageInfo();
 
-
-        fromAddress.setId(1);
-
         fromAddress.setFromStreetOne("1234 Street");
-        fromAddress.setFromZip("1");
+        fromAddress.setFromZip("90231");
         String streetOneValue = fromAddress.getFromStreetOne();
         String originZip = fromAddress.getFromZip();
 
-        toAddress.setId(1);
         toAddress.setToStreetOne("3421 Avenue");
-        toAddress.setToZip("1");
+        toAddress.setToZip("90231");
         String streetTwoValue = toAddress.getToStreetOne();
         String destinationZip = toAddress.getToZip();
 
-        parcel.setPackageInfoId(1);
         parcel.setWeight(32.50);
 
         double weight = parcel.getWeight();
-
-
 
         HashMap toAddressMap = new HashMap<String, Object>();
         toAddressMap.put("street1", streetTwoValue);
@@ -86,7 +77,6 @@ class EnterpriseApplicationTests {
 
         HashMap parcelMap = new HashMap<String, Object>();
         parcelMap.put("weight", weight);
-        //parcelMap.put("predefined_package", predefinedPackage);
 
         shipmentMap.setFromAddress(fromAddressMap);
         shipmentMap.setToAddress(toAddressMap);
@@ -97,86 +87,56 @@ class EnterpriseApplicationTests {
         shipment.put("from_address", shipmentMap.getFromAddress());
         shipment.put("parcel", shipmentMap.getParcel());
 
-        shipmentMap.setId(5);
-        Mockito.when(shipmentMapDAO.findShipById(5)).thenReturn(shipmentMap);
+        //EASYPOST logic
+        com.easypost.model.Shipment shipmentModel = null;
+        //Create easypost shipment
+        try {
+            shipmentModel = com.easypost.model.Shipment.create(shipment);
+        } catch (EasyPostException e) {
+            e.printStackTrace();
+        }
+        ShipmentRate shipmentRate = new ShipmentRate();
+        //Parse Shipment object's rates and save each one to an object.
+        try{
+            var rates = shipmentModel.getRates();
+
+            //For each rate, save to an object.
+            for(Rate rate : rates){
+
+                shipmentRate.setObject(fromAddress.getFromStreetOne());
+                shipmentRate.setCarrier(rate.getCarrier());
+                shipmentRate.setService(rate.getService());
+                shipmentRate.setRate(rate.getRate());
+                shipmentRate.setId(5);
+
+                shipmentRateService.saveRate(shipmentRate);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        Mockito.when(shipmentRateService.findRate(5)).thenReturn(shipmentRate);
     }
 
-    private void whenShipmentDataWithId5(){
-        shipmentMap = shipmentMapService.findShipmentById(5);
+    private void whenShipmentRateDataWithId5(){
+        shipmentRate = shipmentRateService.findRate(5);
     }
 
-    private void thenReturnShipmentWithId5(){
+    private void thenReturnShipmentRateWithId5(){
 
-        int id = shipmentMap.getId();
+        int id = shipmentRate.getId();
         assertEquals(5, id);
     }
 
     @Test
-    void saveShipmentMapToHashmap() throws Exception{
-        givenShipmentMapDataAvailable();
-        whenNewShipmentMapCreated();
-        thenCreateNewShipmentMap();
-    }
-
-    private void whenNewShipmentMapCreated() throws Exception {
-        FromAddress fromAddress = new FromAddress();
-        ToAddress toAddress = new ToAddress();
-        PackageInfo parcel = new PackageInfo();
-
-
-        fromAddress.setId(1);
-        fromAddress.setFromStreetOne("1234 Street");
-        fromAddress.setFromZip("1");
-        toAddress.setId(1);
-        toAddress.setToStreetOne("3421 Avenue");
-        toAddress.setToZip("1");
-        parcel.setPackageInfoId(1);
-        parcel.setWeight(32.50);
-
-
-        String streetTwoValue = toAddress.getToStreetOne();
-        String destinationZip = toAddress.getToZip();
-        String streetOneValue = fromAddress.getFromStreetOne();
-        String originZip = fromAddress.getFromZip();
-        double weight = parcel.getWeight();
-
-
-
-        HashMap toAddressMap = new HashMap<String, Object>();
-        toAddressMap.put("street1", streetTwoValue);
-        toAddressMap.put("zip", destinationZip);
-
-        HashMap fromAddressMap = new HashMap<String, Object>();
-        fromAddressMap.put("street1", streetOneValue);
-        fromAddressMap.put("zip", originZip);
-
-        HashMap parcelMap = new HashMap<String, Object>();
-        parcelMap.put("weight", weight);
-        //parcelMap.put("predefined_package", predefinedPackage);
-
-        shipmentMap.setFromAddress(fromAddressMap);
-        shipmentMap.setToAddress(toAddressMap);
-        shipmentMap.setParcel(parcelMap);
-
-        Map<String, Object> shipment = new HashMap<String, Object>();
-        shipment.put("to_address", shipmentMap.getToAddress());
-        shipment.put("from_address", shipmentMap.getFromAddress());
-        shipment.put("parcel", shipmentMap.getParcel());
-    }
-
-    private void thenCreateNewShipmentMap() throws Exception {
-        ShipmentMap shipmentTwo = shipmentMapService.saveEstimate(shipmentMap);
-        assertEquals(shipmentMap, shipmentTwo);
-    }
-
-    @Test
     void LoadShipmentMaps() throws Exception{
-        givenShipmentMapDataAvailable();
-        whenNewShipmentMapCreated();
+        givenShipmentRateDataAvailable();
+        whenShipmentRateWithId5Exists();
         thenLoadShipmentMaps();
     }
 
     private void thenLoadShipmentMaps() {
-        List<ShipmentMap> shipmentMaps = shipmentMapService.fetchAllShipments();
+        List<ShipmentRate> shipmentRates = shipmentRateService.findAllRates();
     }
 }
